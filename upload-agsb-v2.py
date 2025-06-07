163583502702b653e00bca5881e652e919d0fecb
mazter <61718400+benz0779@users.noreply.github.com>
2025-06-07 17:26:37 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
+USER_NAME = "tmdos"         # 用户名
+UUID = "5f453656-3288-484c-ae0e-2da590ea3b7a"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "asb.mazter.nyc.mn"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiMjZmODZlZmI1NzU1OWMwMDc0NDUzYjZkOGYzOWEzZjEiLCJ0IjoiZmE2ZjYyNjQtMzZiMy00NGY5LTkyMmItZWI5YWQzMDFhNDEwIiwicyI6Ik9URmxPRFl5WkRBdFpHTm1PUzAwTUdVM0xXSmxPRFV0WlRBeU1qWmlZalptTlRRMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
